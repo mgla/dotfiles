f0d9216affebfaa1078e66961c062c151f463560
glatki <glatki@modell-aachen.de>
2014-02-04 11:16:58 +0100

    changes

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FILES="files"
+
+install $FILES/zshrc ~/.zshrc
+install $FILES/vimrc ~/.vimrc
+install $FILES/bashrc ~/.bashrc
