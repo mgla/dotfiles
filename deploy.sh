e30b4a59f71e2caa4588495be05a10097ff513e5
mgla <maik.glatki@gmail.com>
2014-03-04 11:13:37 +0100

    Fixed directory

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #Script for deploying config files. Overwrites everything. Only use if you know what to do.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-FILES="files"
+FILES="$DIR/files"
 
 install $FILES/zshrc ~/.zshrc
 install $FILES/vimrc ~/.vimrc
