2f439ea27fe62547f647bb25dfa0b38ddfa3e7d1
Maik Glatki <maik.glatki@gmail.com>
2015-03-09 13:35:59 +0100

    Set correct mode during deployment. Use set -u.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #Script for deploying config files. Overwrites everything. Only use if you know what to do.
+set -u
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FILES="$DIR/files"
+INSTALL="install --mode=644"
 
 set -x
-install $FILES/zshrc ~/.zshrc
-install $FILES/vimrc ~/.vimrc
+$INSTALL $FILES/zshrc ~/.zshrc
+$INSTALL $FILES/vimrc ~/.vimrc
 touch ~/.vimrc.more
-install $FILES/bashrc ~/.bashrc
-install $FILES/screenrc ~/.screenrc
-install $FILES/zprofile ~/.zprofile
+$INSTALL $FILES/bashrc ~/.bashrc
+$INSTALL $FILES/screenrc ~/.screenrc
+$INSTALL $FILES/zprofile ~/.zprofile
