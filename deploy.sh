240cbbc2276c2a6e4c8599260d80b7359dcd9716
Maik Glatki <glatki@modell-aachen.de>
2016-01-19 14:03:06 +0100

    Change autoinstaller to install pathogen.

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,21 @@ INSTALL="install --mode=644"
 
 set -x
 $INSTALL $FILES/zshrc ~/.zshrc
-$INSTALL $FILES/vimrc ~/.vimrc
-touch ~/.vimrc.more
 $INSTALL $FILES/bashrc ~/.bashrc
 $INSTALL $FILES/screenrc ~/.screenrc
 $INSTALL $FILES/zprofile ~/.zprofile
+
+# Vim
+touch ~/.vimrc.more
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+$INSTALL $FILES/vimrc ~/.vimrc
+$INSTALL $FILES/vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+
+cd ~/.vim/bundle;
+if [[ -d vim-gitgutter ]]; then
+	cd vim-gitgutter;
+	git pull -q;
+else
+	git clone git://github.com/airblade/vim-gitgutter.git
+fi
